d1cd35b531ed4c9276dd9f9e3cc5d1c49d9b05a2
SamuelCallWind <117657697+SamuelCallWind@users.noreply.github.com>
2023-05-31 10:08:21 +0200

    Add files via upload
    Small change on the Polish letter changer 
    + adding one letter that was still wrong (the ą, because apparently there were 2 problematic letters for this one)

--- Shorter_Polish_letter_changer.js
+++ Shorter_Polish_letter_changer.js
@@ -1,40 +1,29 @@
-var fs = require('fs');
-const path = '/home/sam/Downloads/The.Matrix.1999.REMASTERED.1080p.BluRay.x265-RARBG.Polish.Official.srt'
-var iconv = require('iconv-lite');
+const fs = require('fs');
+const path = '/home/sam/Downloads/The.Matrix.1999.REMASTERED.1080p.BluRay.x265-RARBG.Polish.Official.srt';
+const iconv = require('iconv-lite');
 
 try {
-    const buffer = fs.readFileSync(path);
+  const buffer = fs.readFileSync(path);
+  const data = iconv.decode(buffer, 'ISO-8859-2');
 
-    const data = iconv.decode(buffer, 'ISO-8859-2');
+  const replacements = {
+    '³': 'ł',
+    '': 'ś',
+    '¹': 'ą',
+    'æ': 'ć',
+    '': 'ź',
+    'š': 'ą',
+    '¿': 'ż',
+    'ê': 'ę',
+    'ñ': 'ń',
+    '': 'Ś',
+  };
 
-    var new_file = '';
+  var new_file = data.split('').map(char => replacements[char] || char).join('');
+  var buffer2 = iconv.encode(new_file, 'ISO-8859-2');
 
-    for(let i = 0; i < data.length; i++){
-        let char = data[i];
-
-        var replacement = {
-            '³' : 'ł',
-            '' : 'ś',
-            '¹' : 'ą',
-            'æ' : 'ć',
-            '' : 'ź',
-            '¿' : 'ż',
-            'ê' : 'ę',
-            'ñ' : 'ń',
-            '' : 'Ś',
-        };
-
-        if (char in replacement) {
-            char = replacement[char];
-        }
-        new_file += char;
-
-    }
-
-} catch (err) {
-    console.error('Error reading file', err);
+} catch(err) {
+  console.error('unable to read the file', err);
 }
-
-var buffer = iconv.encode(new_file, 'ISO-8859-2');
 
 fs.writeFileSync(path + '_changed', new_file);
